481b794d319227f90e13970351ea5121ac9d70b2
Namal <namal@pop-os.localdomain>
2019-05-24 23:20:41 +0100

    Print pi with colours

--- js/print-pi.js
+++ js/print-pi.js
@@ -1,16 +1,49 @@
-// Setup
-// Update text every 30 milliseconds
-const piParagraph = document.getElementById("pi");
-var updateTextIntervalId = setInterval(updatePiText, 30);
+// Colours
+const normalDigitColour = "#424242";
+const newDigitColour = "#E8847F";
 
-// Stop printing when button is clicked
-document.getElementById("stop-button").addEventListener("click", stopPrint);
+// Print a digit of PI every interval
+const intervalMs = 100;
+const piString = Math.PI.toFixed(48).toString();
 
-// Functions
-function updatePiText() {
-    piParagraph.innerText += 3;
+const maxPosition = 49;
+let currentPosition = 0;
+
+// Start
+const piElement = document.getElementById("pi");
+printPi(piElement);
+
+function printPi(printElement) {
+    const digit = piString[currentPosition];
+
+    // Reset colours
+    printElement.innerHTML = printElement.innerHTML.replace(newDigitColour, normalDigitColour);
+
+    printElement.innerHTML += "<span style='color: " + newDigitColour + "; '>" + digit + "</span>";
+
+    // Continue printing if there are digits left
+    if (currentPosition < maxPosition) {
+        currentPosition++;
+        setTimeout(function () {
+            printPi(printElement);
+        }, intervalMs);
+    }
+    else {
+        // Reset colours
+        printElement.innerHTML = printElement.innerHTML.replace(newDigitColour, normalDigitColour);
+    }
 }
 
-function stopPrint() {
-    clearInterval(updateTextIntervalId);
+// Restart printing when button is clicked
+document.getElementById("reset-button").addEventListener("click", restartPrint);
+
+function restartPrint() {
+    const piElement = document.getElementById("pi");
+
+    // Reset state
+    currentPosition = 0;
+    piElement.innerText = "";
+
+    // Start printing again
+    printPi(piElement);
 }
